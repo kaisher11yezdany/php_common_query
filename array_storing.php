93bb9998e2f92932359291fbff704fe0592aa9df
kaisher Yezdany <38363760+kaisher11yezdany@users.noreply.github.com>
2022-08-06 10:04:00 +0530

    Create array_storing.php

---
+++ array_storing.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$storing = array(); 
+$storing[]="a";
+$storing[]="b";
+$storing[]="c";
+$storing[]="d";
+
+
+foreach($storing as $storing)
+{
+	echo $storing."<br>";
+}
+?>
+
+</body>
+</html>
